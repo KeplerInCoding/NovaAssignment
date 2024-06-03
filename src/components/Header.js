d6afe41a5204ec134eacd7199636dbdf6aebbe90
KeplerInCoding <shreyakv8@gmail.com>
2024-06-03 06:20:07 +0530

    fb

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='fixed top-0 w-screen'>
-        <div className='flex place-content-between p-4 px-10 items-center'>
+        <div className='flex place-content-between gap-10 p-4 px-10 items-center'>
             <div className='flex gap-2'>
                 <div className='h-4 w-4 bg-black rounded-full'></div>
                 <div className='h-4 w-4 bg-black rounded-full'></div>
